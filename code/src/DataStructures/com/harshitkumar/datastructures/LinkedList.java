31b0b67f873c20d9f131130f0aaaefdf1d32b530
Harshit Kumar <harshitkumar.hk@outlook.com>
2019-01-29 12:13:32 -0600

    Added : LinkedList with test cases

---
+++ code/src/DataStructures/com/harshitkumar/datastructures/LinkedList.java
@@ -0,0 +1,49 @@
+package com.harshitkumar.datastructures;
+
+public class LinkedList {
+	/**
+	 * 	Define a Node
+	 *  data : an integer data will be stored in LinkedList
+	 *  next : a Pointer to next node
+	 * @author Harshit Kumar
+	 */
+	public class Node {
+		int data;
+		Node next;
+
+		public Node(int data) {
+			this.data = data;
+		}
+	}
+
+	public Node head;
+
+	// Add the data to the front of the Linked List
+	public void addFront(int data) {
+	}
+	// Get the first node data of the Linked List
+	public int getFirst() {	
+		return 0;
+	}
+	// Get the last node data of the Linked List
+	public int getLast() {
+		return 0;
+	}
+	// Add the data to the back of the Linked List
+	public int addBack(int data) {
+		return 0;
+	}
+	// get the size of the Linked List
+	public int size() {
+		return 0;
+	}
+	// Clear the data of the Linked List
+	public void clear() {
+	}
+	// Delete the data from the Linked List
+	public void deleteValue(int data) {
+	}
+	// Print the Linked List
+	public void print() {
+	}
+}
---
+++ code/src/DataStructuresTest/com/harshitkumar/datastructures/LinkedListTest.java
@@ -0,0 +1,58 @@
+package com.harshitkumar.datastructures;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LinkedListTest {
+
+	private LinkedList linkedList;
+
+	@Before
+	public void setUp() throws Exception {
+		linkedList = new LinkedList();
+	}
+
+	// Add the data to the front of the Linked List
+	@Test
+	public void AddFront(int data) {
+		fail("Not yet implemented");
+	}
+	// Get the first node data of the Linked List
+	@Test	
+	public void GetFirst() {	
+		fail("Not yet implemented");
+	}
+	// Get the last node data of the Linked List
+	@Test	
+	public void GetLast() {
+		fail("Not yet implemented");
+	}
+	// Add the data to the back of the Linked List
+	@Test	
+	public void AddBack(int data) {
+		fail("Not yet implemented");
+	}
+	// get the size of the Linked List
+	@Test	
+	public void Size() {
+		fail("Not yet implemented");
+	}
+	// Clear the data of the Linked List
+	@Test	
+	public void clear() {
+		fail("Not yet implemented");	
+	}
+	// Delete the data from the Linked List
+	@Test	
+	public void deleteValue(int data) {
+		fail("Not yet implemented");	
+	}
+	// Print the Linked List
+	@Test	
+	public void print() {
+		fail("Not yet implemented");	
+	}
+
+}
