8307b6475a48de5d8f82b17c4c7a404b750e8e1f
Harshit Kumar <harshitkumar.hk@outlook.com>
2019-01-31 18:38:14 -0600

    Added : Complete LinkedList code with test cases
    addFront(int data)
    int getFirst()
    int getLast()
    addBack(int data)
    int size()
    void clear()
    void deleteValue(int data)
    void print()

--- code/src/DataStructures/com/harshitkumar/datastructures/LinkedList.java
+++ code/src/DataStructures/com/harshitkumar/datastructures/LinkedList.java
@@ -20,30 +20,129 @@ public class LinkedList {
 
 	// Add the data to the front of the Linked List
 	public void addFront(int data) {
+		
+		//Create a new Node
+		Node newNode = new Node(data);
+		
+		//Special Case : If head is pointing to nothing make it the first node
+		if(head == null) {
+			head = newNode;
+			return;
+		}
+		//Set the newNode's next pointer to the current head node
+		newNode.next = head;
+		
+		//Set the head to point to the newNode
+		head = newNode;
 	}
 	// Get the first node data of the Linked List
 	public int getFirst() {	
-		return 0;
+		//check if the head
+				if(head == null) {
+					throw new IllegalStateException("Linked List empty");
+				}
+		return head.data;
 	}
 	// Get the last node data of the Linked List
 	public int getLast() {
-		return 0;
+		//check if the head
+		if(head == null) {
+			throw new IllegalStateException("Linked List empty");
+		}
+		// goto last node
+		
+		Node current = head;
+		//return last node data
+		while(current.next != null) {
+			current = current.next;
+		}
+		return current.data;
 	}
 	// Add the data to the back of the Linked List
-	public int addBack(int data) {
-		return 0;
+	public void addBack(int data) {
+	
+		//create the new Node
+		Node newNode = new Node(data);
+		//Check if head is empty
+		if(head==null) {
+			head = newNode;
+			return;
+		}
+		//Goto Last node
+		Node current = head;
+		
+		while(current.next != null) {
+			current= current.next;
+		}
+		//last node next point to new Node
+		current.next = newNode;
 	}
 	// get the size of the Linked List
 	public int size() {
-		return 0;
+
+		//Check if head is null
+		if(head ==null) {
+			return 0;
+		}
+
+		//Brute Force Method to find the size
+		Node current = head;
+		int size = 1;
+		while(current.next != null) {
+			size++;
+			current = current.next;
+		}
+		return size;
 	}
 	// Clear the data of the Linked List
 	public void clear() {
+		
+		//Just make the head point to null. 
+		//Java Garbage collector will free up space when it finds unlinked Nodes 
+		
+		head = null;
 	}
 	// Delete the data from the Linked List
 	public void deleteValue(int data) {
+		
+		//check if the head is null
+		if(head == null) {
+			throw new IllegalStateException("Linked List empty"); 
+		}
+		//check if the head is only node and contains the value
+
+			if(head.data == data) {
+				head = head.next;
+				return;
+			}
+
+		//goto the required node containing the data
+		Node current = head;
+		
+		while(current.next != null) {
+			if(current.next.data == data) {
+				// move the previous node next pointer to required nodes next node.
+				current.next = current.next.next;
+				return;		
+			}
+			current =current.next;
+		}
+		
 	}
 	// Print the Linked List
 	public void print() {
+		//Check if head is null
+		if(head == null) {
+			System.out.println("Linked List empty");
+		}
+
+		//Brute Force Method to find the size
+		Node current = head;
+		System.out.print("head --> ");
+		while(current != null) {
+			System.out.print("|"+current.data+"| -->");
+			current = current.next;
+		}
+		System.out.println(" null");
 	}
 }
--- code/src/DataStructuresTest/com/harshitkumar/datastructurestests/LinkedListTest.java
+++ code/src/DataStructuresTest/com/harshitkumar/datastructurestests/LinkedListTest.java
@@ -1,6 +1,8 @@
 package com.harshitkumar.datastructurestests;
 
 import static org.junit.Assert.*;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -18,42 +20,93 @@ public class LinkedListTest {
 	// Add the data to the front of the Linked List
 	@Test
 	public void AddFront() {
-		fail("Not yet implemented");
+		linkedList.addFront(1);
+		linkedList.addFront(2);
+		linkedList.addFront(3);
 	}
 	// Get the first node data of the Linked List
 	@Test	
 	public void GetFirst() {	
-		fail("Not yet implemented");
+		linkedList.addFront(1);
+		linkedList.addFront(2);
+		linkedList.addFront(3);
+		linkedList.print();
+		Assert.assertEquals(3, linkedList.getFirst());
 	}
 	// Get the last node data of the Linked List
 	@Test	
 	public void GetLast() {
-		fail("Not yet implemented");
+		linkedList.addFront(1);
+		linkedList.addFront(2);
+		linkedList.addFront(3);
+		linkedList.print();
+		Assert.assertEquals(1, linkedList.getLast());
 	}
 	// Add the data to the back of the Linked List
 	@Test	
 	public void AddBack() {
-		fail("Not yet implemented");
+		linkedList.addFront(1);
+		linkedList.addFront(2);
+		linkedList.addFront(3);
+		linkedList.addBack(10);
+		linkedList.print();
+		Assert.assertEquals(10, linkedList.getLast());
 	}
 	// get the size of the Linked List
 	@Test	
 	public void Size() {
-		fail("Not yet implemented");
+		
+		linkedList.print();
+		Assert.assertEquals(0, linkedList.size());
+		
+		linkedList.addFront(1);
+		linkedList.addFront(2);
+		linkedList.addFront(3);
+		linkedList.addBack(10);
+		linkedList.addBack(20);
+		linkedList.addBack(30);
+		linkedList.print();
+		Assert.assertEquals(6, linkedList.size());
 	}
 	// Clear the data of the Linked List
 	@Test	
 	public void clear() {
-		fail("Not yet implemented");	
+		linkedList.addFront(1);
+		linkedList.addFront(2);
+		linkedList.addFront(3);
+		linkedList.addBack(4);
+		linkedList.addBack(5);
+		linkedList.addBack(6);
+		linkedList.print();
+		
+		linkedList.clear();
+		linkedList.print();
+		Assert.assertEquals(0, linkedList.size());	
 	}
 	// Delete the data from the Linked List
 	@Test	
 	public void deleteValue() {
-		fail("Not yet implemented");	
+			
+		linkedList.addFront(1);
+		linkedList.addFront(2);
+		linkedList.addFront(3);
+		linkedList.addBack(4);
+		linkedList.addBack(5);
+		linkedList.addBack(6);
+		linkedList.print();
+		
+		linkedList.deleteValue(3);
+		linkedList.print();
+		
+		Assert.assertEquals(2,linkedList.getFirst());
+		
 	}
+
+	
 	// Print the Linked List
-	@Test	
-	public void print() {
-		fail("Not yet implemented");	
-	}
+//	@Test	
+//	public void print() {
+//		fail("Not yet implemented");	
+//	}
 
 }
