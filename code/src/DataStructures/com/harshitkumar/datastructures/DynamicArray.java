92abd315c8a61820c0cb458767356d63bb99a37f
Harshit Kumar <harshitkumar.hk@outlook.com>
2019-02-02 16:06:45 -0600

    Added Required functions and test cases to start coding

--- code/src/DataStructures/com/harshitkumar/datastructures/DynamicArray.java
+++ code/src/DataStructures/com/harshitkumar/datastructures/DynamicArray.java
@@ -1,8 +1,77 @@
 package com.harshitkumar.datastructures;
 
-public class DynamicArray {
+public class DynamicArray<String>  {
 
+	private Object[] data;
+	private int size;				/** to track the number of elements present in Array **/
+	private int initialCapacity;	/** to check the size of the overall Array we have **/
 	
+	//******Create the initial capacity Array******* 
+	public DynamicArray(int intialCapacity) {
+		this.initialCapacity = intialCapacity;
+		data = new Object[intialCapacity];
+	}
+	
+	
+	/**
+	 * Following logics will be demonstrated in Dynamic Array:
+	 * get(int index)  - return String
+	 * set(int index, String value)
+	 * insert(int index, String value)
+	 * delete(int index)
+	 * isEmtpy() - return Boolean
+	 * Contains(String value) - return Boolean
+	 * resize()
+	 * size() - return int
+	 * add(String value)           <-- to insert at the end of the array
+	 */
+	
+	public String get(int index) {
+		return null;
+    }
+
+    public void set(int index, String value) {
+
+    }
+
+    public void insert(int index, String value) {
+
+        // Check size
+
+        // Copy up
+
+        // Insert
+    }
+
+    private void resize() {
+
+    }
+
+    public int size() {
+    	return 0;
+    }
+
+    public void print() {
+
+    }
+
+    // Some array implementations offer methods like insert (also known as pushBack).
+    // Which is like insert, only in does the insert at the end of the array.
+    public void add(String value) {
+
+    }
+
+    public void delete(int index) {
+        
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean Contains(String value) {
+        return false;
+    }
 	
 	
 }
--- code/src/DataStructuresTest/com/harshitkumar/datastructurestests/DynamicArrayTest.java
+++ code/src/DataStructuresTest/com/harshitkumar/datastructurestests/DynamicArrayTest.java
@@ -1,5 +1,45 @@
 package com.harshitkumar.datastructurestests;
 
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.harshitkumar.datastructures.DynamicArray;
+
 public class DynamicArrayTest {
 
+	private DynamicArray array;
+	
+	@Before
+	public void Setup() throws Exception {
+		array = new DynamicArray<String>(2); 
+	}
+	
+	@Test
+    public void GetAndSet() throws Exception {
+		fail("To Be implemented");
+    }
+
+    @Test
+    public void Insert() throws Exception {
+    	fail("To Be implemented");
+    }
+
+    @Test
+    public void Delete() throws Exception {
+    	fail("To Be implemented");
+    }
+
+    @Test
+    public void isEmpty() throws Exception {
+    	fail("To Be implemented");
+    }
+
+    @Test
+    public void Contains() throws Exception {
+        fail("To Be implemented");
+    }
+	
+	
 }
