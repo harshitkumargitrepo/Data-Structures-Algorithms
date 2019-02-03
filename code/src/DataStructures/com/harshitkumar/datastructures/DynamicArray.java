008ada13ac8a0171a38cc309004b3bf9414a563e
Harshit Kumar <harshitkumar.hk@outlook.com>
2019-02-02 19:00:22 -0600

    added: complete test for dynamic array

--- code/src/DataStructures/com/harshitkumar/datastructures/DynamicArray.java
+++ code/src/DataStructures/com/harshitkumar/datastructures/DynamicArray.java
@@ -1,18 +1,19 @@
 package com.harshitkumar.datastructures;
 
+
 public class DynamicArray<String>  {
 
 	private Object[] data;
 	private int size;				/** to track the number of elements present in Array **/
 	private int initialCapacity;	/** to check the size of the overall Array we have **/
-	
+
 	//******Create the initial capacity Array******* 
 	public DynamicArray(int intialCapacity) {
 		this.initialCapacity = intialCapacity;
 		data = new Object[intialCapacity];
 	}
-	
-	
+
+
 	/**
 	 * Following logics will be demonstrated in Dynamic Array:
 	 * get(int index)  - return String
@@ -25,53 +26,137 @@ public class DynamicArray<String>  {
 	 * size() - return int
 	 * add(String value)           <-- to insert at the end of the array
 	 */
-	
-	public String get(int index) {
-		return null;
-    }
 
-    public void set(int index, String value) {
+	public String get(int index) throws Exception{
+		//Get the value at specified Index
+		if(index>=initialCapacity) {
+			throw new IllegalStateException("Index is out of Bound"); 
+		}
+		System.out.println("\n Value at array["+index+"]="+data[index].toString());
+		return  (String) data[index];             /***   O(1)   *****/
 
-    }
+	}
 
-    public void insert(int index, String value) {
+	public void set(int index, String value){
+		//Set the value at the required index
+		data[index] = value;               /***   O(1)   *****/
+	}
 
-        // Check size
+	public void insert(int index, String value) {
 
-        // Copy up
+//		System.out.println("\n Initial State before inserting " + value + ". Array has size = "+size()+" elements"+" and Capacity = "+ initialCapacity);
+//		print();
+		
+		// Check size if full or index is out of bound
+		while(size == initialCapacity || index >= initialCapacity) {
+			if(index >= initialCapacity) {
+				System.out.println("\nIndex out of bound : Increasing capacity to 2 times");
+			}else if(size == initialCapacity){
+				System.out.println("\ninitial size full  : Increasing capacity to 2 times");
+			}
+			resize();
 
-        // Insert
-    }
+			// Copy up
+			System.out.println();
+			for(int i = initialCapacity-1; i>index; i--) {
+				this.data[i] = this.data[i-1];
+			}
+		}
+		
+		// Insert
+		set(index,value);
+		size++;
+//		System.out.println("\n Final State after inserting " + value + ". Array has new size = " + size()+" elements and Capacity = "+ initialCapacity);
+//		print();
+//		System.out.println("\n**************************************");
+	}
 
-    private void resize() {
+	private void resize() {
+		Object[] newData = new Object[initialCapacity*2];
+		for(int i= 0; i<initialCapacity;i++) {
+			newData[i] = this.data[i];
+		}
+		this.data = newData;
+		initialCapacity = initialCapacity*2;
+	}
 
-    }
+	public int size() {
+		return size;
+	}
 
-    public int size() {
-    	return 0;
-    }
+	public void print() {
+		int loopsize = initialCapacity-1;
+		while(loopsize>=0) {
+			System.out.print("  array["+(initialCapacity-1-loopsize)+"]="+ data[initialCapacity-1-loopsize]+"  ");
+			loopsize--;
+		}
+	}
 
-    public void print() {
+	// Some array implementations offer methods like insert (also known as pushBack).
+	// Which is like insert, only in does the insert at the end of the array.
+	public void add(String value) {
 
-    }
+//		System.out.println("\n Initial State before inserting " + value + ". Array has size = "+size()+" elements"+" and Capacity = "+ initialCapacity);
+//		print();
 
-    // Some array implementations offer methods like insert (also known as pushBack).
-    // Which is like insert, only in does the insert at the end of the array.
-    public void add(String value) {
+		    //Check size if full 
+		if(size == initialCapacity) {
+			System.out.println("\n ------> Size of array full : Increasing capacity to 2 times");
+			resize();		
+		}
+		set(size,value);
+		size++;
+		
+//		System.out.println("\n Final State after inserting " + value + ". Array has new size = " + size()+" elements and Capacity = "+ initialCapacity);
+//		print();
+//		System.out.println("\n**************************************");
+	}
 
-    }
+	public void delete(int index) {
 
-    public void delete(int index) {
-        
-    }
+		System.out.println("\n Initial State before deleting at index " + index + ". Array has size = "+size()+" elements"+" and Capacity = "+ initialCapacity);
+		print();
 
-    public boolean isEmpty() {
-        return false;
-    }
+		//Check if index is out of bound
+		if(index>=initialCapacity) {
+			throw new IllegalStateException("Index out of bound");
+		}
+		//Shift one element to the left of the index
+		int i = index;
+		while(i<initialCapacity-1) {
+			this.data[i] = this.data[i+1];
+			i++;
+		}
+		this.data[i] = null;
+		size--;
+		System.out.println("\n Final State after deleting at index " + index + ". Array has new size = " + size()+" elements and Capacity = "+ initialCapacity);
+		print();
+		System.out.println("\n**************************************");
+	}
 
-    public boolean Contains(String value) {
-        return false;
-    }
-	
-	
+	public boolean isEmpty() {
+
+//		if(size() == 0)
+//			return true;
+//		else
+//			return false;
+		
+		return size == 0;
+	}
+
+	public boolean Contains(String value) {
+		print();
+		System.out.println();
+		int i = initialCapacity-1;
+		while(i>=0) {
+			if(data[i]== value) {
+				System.out.println("Item "+value+" found at array["+i+"]");
+				return true;
+			}
+			i--;
+		}
+		return false;
+	}
+
+
 }
--- code/src/DataStructuresTest/com/harshitkumar/datastructurestests/DynamicArrayTest.java
+++ code/src/DataStructuresTest/com/harshitkumar/datastructurestests/DynamicArrayTest.java
@@ -2,6 +2,7 @@ package com.harshitkumar.datastructurestests;
 
 import static org.junit.Assert.fail;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,37 +10,81 @@ import com.harshitkumar.datastructures.DynamicArray;
 
 public class DynamicArrayTest {
 
-	private DynamicArray array;
+	private DynamicArray<String> array;
 	
 	@Before
 	public void Setup() throws Exception {
 		array = new DynamicArray<String>(2); 
 	}
 	
+	
 	@Test
     public void GetAndSet() throws Exception {
-		fail("To Be implemented");
-    }
+		//		fail("To Be implemented");
+		array.set(0, "A");	
+		array.set(1, "B");		
+		Assert.assertEquals("B", array.get(1));
+	}
 
     @Test
     public void Insert() throws Exception {
-    	fail("To Be implemented");
+    	//fail("To Be implemented");
+    	array.insert(0,"A");
+    	array.insert(1,"B");
+    	// Check if the index is out of bound
+    	array.insert(2,"C");
+    	array.insert(3,"D");
+    	//Check if the size is full
+    	array.insert(3,"C-D");
+    	
+    	Assert.assertEquals("C", array.get(2));	
+    	Assert.assertEquals("D", array.get(4));
+    	
     }
 
     @Test
     public void Delete() throws Exception {
-    	fail("To Be implemented");
+    	array.add("A");
+    	array.add("B");
+    	array.insert(2, "C");
+    	array.add("D");
+    	array.add("E");
+    	array.insert(6, "F");
+    	array.delete(0);
+    	array.delete(0);
+    	
     }
 
     @Test
     public void isEmpty() throws Exception {
-    	fail("To Be implemented");
+    	
+    	//fail("To Be implemented");
+    	array.insert(0,"A");
+    	array.insert(1,"B");
+    	array.insert(1,"C");
+    	Assert.assertFalse(array.isEmpty());
     }
 
     @Test
     public void Contains() throws Exception {
-        fail("To Be implemented");
+        //fail("To Be implemented");
+    	array.add("A");
+    	array.add("B");
+    	array.insert(4, "C");
+    	array.add("D");
+    	Assert.assertTrue("Value not found",array.Contains("C"));
     }
 	
+	
+    @Test
+    public void add() throws Exception {
+        //fail("To Be implemented");
+    	array.add("A");
+    	array.add("B");
+    	array.insert(4, "C");
+    	array.add("D");
+    	Assert.assertEquals("D", array.get(array.size()-1));
+    	
+    }
 	
 }
